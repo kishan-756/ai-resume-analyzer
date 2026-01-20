137d4018f0746286810301cf501d3b40814b94c1
kishan-756 <kishan0071957@gmail.com>
2026-01-20 12:45:02 +0530

    pushing works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const uploadFile = async () => {
-    if (!file) return alert("Please select a resume");
+    if (!file) return alert("Please select a PDF resume");
 
     const formData = new FormData();
     formData.append("resume", file);
